2050da42cbce9c35a05ef5f2a1f647938f699331
Frankdesantiago <fco.de.santiago.d@gmail.com>
2015-06-10 09:49:04 -0700

    [REF] agregando color a vistas calender

--- openacademy/model/openacademy_session.py
+++ openacademy/model/openacademy_session.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from datetime import timedelta
+
 from openerp import api, fields, models
 
 
@@ -13,6 +15,7 @@ class Session(models.Model):
     seats = fields.Integer(string="Number of Seats")
     active = fields.Boolean(default=True)
 
+
     instructor_id = fields.Many2one('res.partner', string="Instructor",
                                     domain=['|',
                                          ("instructor", "=", True),
@@ -22,6 +25,8 @@ class Session(models.Model):
         ondelete='cascade', string="Course", required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
     taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
+    end_date = fields.Date(string="End Date", store=True,
+        compute='_get_end_date', inverse='_set_end_date')
 
     @api.one
     @api.depends('seats', 'attendee_ids')
@@ -53,3 +58,27 @@ class Session(models.Model):
     def _check_instructor_not_in_attendees(self):
         if self.instructor_id and self.instructor_id in self.attendee_ids:
             raise exceptions.ValidationError("A session's instructor can't be an attendee")
+    @api.one
+    @api.depends('start_date', 'duration')
+    def _get_end_date(self):
+        if not (self.start_date and self.duration):
+            self.end_date = self.start_date
+            return
+
+        # Add duration to start_date, but: Monday + 5 days = Saturday, so
+        # subtract one second to get on Friday instead
+        start = fields.Datetime.from_string(self.start_date)
+        duration = timedelta(days=self.duration, seconds=-1)
+        self.end_date = start + duration
+
+    @api.one
+    def _set_end_date(self):
+        if not (self.start_date and self.end_date):
+            return
+
+        # Compute the difference between dates, but: Friday - Monday = 4 days,
+        # so add one day to get 5 days instead
+        start_date = fields.Datetime.from_string(self.start_date)
+        end_date = fields.Datetime.from_string(self.end_date)
+        self.duration = (end_date - start_date).days + 1
+
